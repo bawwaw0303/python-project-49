bdaccf310c314aef3d35fc60c7d2bc3e256d7b6e
bawwaw0303 <chief.sergei93@yandex.com>
2026-06-21 15:12:06 +0300

    add linter fix for brain_games/scripts/brain_progression.py and brain_games/scripts/brain_progression.py

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -3,11 +3,10 @@ from random import randint
 description = 'What number is missing in the progression?'
 
 
-
 def generate_progression():
     """Функция для генерации прогрессии"""
     start_progression_number = randint(0, 10)
-    step_of_progression = randint(1,5)
+    step_of_progression = randint(1, 5)
     length_of_progression = randint(5, 10)
 
     progression_list = [start_progression_number]
@@ -17,15 +16,17 @@ def generate_progression():
 
     return progression_list
 
+
 def generate_round():
     progression = generate_progression()
     random_index = randint(0, len(progression) - 1)
 
     answer = progression[random_index]
     progression[random_index] = '..'
-    question = ' '.join(map(str,progression))
+    question = ' '.join(map(str, progression))
 
     return question, str(answer)
+
 
 game_dict = {
     'description': description,
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,5 +1,6 @@
-from brain_games.games.game_progression import game_dict
 from brain_games.engine import game_engine
+from brain_games.games.game_progression import game_dict
+
 
 def main():
     game_engine(game_dict)
